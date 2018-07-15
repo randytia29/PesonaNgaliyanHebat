81a6ce23c310d059251946facaabf17207078083
randytia29 <randytia.akbar@gmail.com>
2018-07-15 10:23:00 +0700

    15juli

--- app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/KulinerActivity.java
+++ app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/KulinerActivity.java
@@ -1,83 +1,16 @@
 package com.hebat.kecamatanngaliyan.pesonangaliyanhebat;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 public class KulinerActivity extends AppCompatActivity {
-
-    private ViewPager mPager;
-    private int[] layouts = {R.layout.first_slide, R.layout.second_slide, R.layout.third_slide};
-    LinearLayout kuliner1;
-    private MpagerAdapter mpagerAdapter;
-
-    private LinearLayout Dots_layout;
-    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*if (Build.VERSION.SDK_INT >= 19) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        } else {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }*/
-
         setContentView(R.layout.activity_kuliner);
 
-        mPager = (ViewPager) findViewById(R.id.viewPager);
-        mpagerAdapter = new MpagerAdapter(layouts, this);
-        mPager.setAdapter(mpagerAdapter);
-
-        Dots_layout = (LinearLayout) findViewById(R.id.dotsLayout);
-        createDots(0);
-
-        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                createDots(position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-    }
-
-    private void createDots(int current_position) {
-        if (Dots_layout != null)
-            Dots_layout.removeAllViews();
-
-        dots = new ImageView[layouts.length];
-
-        for (int i = 0; i < layouts.length; i++) {
-            dots[i] = new ImageView(this);
-            if (i == current_position) {
-                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.active_dots));
-            } else {
-                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.default_dots));
-            }
-
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            params.setMargins(4, 0, 4, 0);
-
-            Dots_layout.addView(dots[i], params);
-        }
     }
 
     @Override
