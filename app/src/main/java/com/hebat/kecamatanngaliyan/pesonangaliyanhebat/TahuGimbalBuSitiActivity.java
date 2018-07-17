649c020ce44de4e9ac06e85b025fcd50bb9270f2
randytia <randytia.akbar@gmail.com>
2018-07-17 09:56:02 +0700

    kuliner pertamax

---
+++ app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/TahuGimbalBuSitiActivity.java
@@ -0,0 +1,13 @@
+package com.hebat.kecamatanngaliyan.pesonangaliyanhebat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TahuGimbalBuSitiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tahu_gimbal_bu_siti);
+    }
+}
