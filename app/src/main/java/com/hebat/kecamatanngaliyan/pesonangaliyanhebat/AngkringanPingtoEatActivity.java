1c6297f49d28814ce132c666ca3de796a0e4da1a
randytia <randytia.akbar@gmail.com>
2018-07-17 12:11:15 +0700

    kuliner 2

---
+++ app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/AngkringanPingtoEatActivity.java
@@ -0,0 +1,13 @@
+package com.hebat.kecamatanngaliyan.pesonangaliyanhebat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AngkringanPingtoEatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_angkringan_pingto_eat);
+    }
+}
--- app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/KulinerActivity.java
+++ app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/KulinerActivity.java
@@ -3,13 +3,35 @@ package com.hebat.kecamatanngaliyan.pesonangaliyanhebat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class KulinerActivity extends AppCompatActivity {
+
+    CardView cardTahuGimbalBuSiti, cardAngkringanPingtoEat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuliner);
+
+        cardTahuGimbalBuSiti = findViewById(R.id.tahuGimbalBuSiti);
+        cardTahuGimbalBuSiti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KulinerActivity.this, TahuGimbalBuSitiActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardAngkringanPingtoEat = findViewById(R.id.angkringanpingtoeat);
+        cardAngkringanPingtoEat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KulinerActivity.this, AngkringanPingtoEatActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
