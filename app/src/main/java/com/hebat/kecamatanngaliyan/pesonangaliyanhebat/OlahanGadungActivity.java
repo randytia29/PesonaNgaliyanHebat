5dd7d23f3575a73a4c842ee173c243b75939245a
randytia29 <randytia.akbar@gmail.com>
2019-05-28 11:31:19 +0700

    update

--- app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/OlahanGadungActivity.java
+++ app/src/main/java/com/hebat/kecamatanngaliyan/pesonangaliyanhebat/OlahanGadungActivity.java
@@ -1,6 +1,5 @@
 package com.hebat.kecamatanngaliyan.pesonangaliyanhebat;
 
-import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.AppBarLayout;
@@ -10,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class OlahanGadungActivity extends AppCompatActivity {
